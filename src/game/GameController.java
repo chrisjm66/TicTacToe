64b32ddedd655426fc9517f36b7d2e8d8aa30e13
Christopher Mangan <chrisjm66@gmail.com>
2025-02-02 22:47:54 -0500

    Init gamecontroller, UI work

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -1,14 +1,58 @@
 package game;
 
-import javafx.scene.layout.Pane;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
-public class GameController extends Pane {
+public class GameController extends VBox {
     // Constants
-    private int TILE_WIDTH;
-    private int TILE_HEIGHT;
+    private int TILE_WIDTH = 150;
+    private int TILE_HEIGHT = 150;
+
+    // Objects
+    private Text headerText;
+    private GridPane grid;
+    private Button[] gameButtons;
 
     public GameController() {
+        headerText = new Text("Click any tile to start a game");
+        grid = new GridPane();
+        gameButtons = new Button[9];
 
+        setupGameButtons();
+        setupVBox();
+    }
+
+    private void setupGameButtons() {
+        for(int i = 0; i < gameButtons.length; i++){
+            gameButtons[i] = new Button("X");
+            gameButtons[i].setPrefWidth(TILE_WIDTH);
+            gameButtons[i].setPrefHeight(TILE_HEIGHT);
+            gameButtons[i].setFont(Font.font(100));
+            gameButtons[i].setFocusTraversable(false);
+            
+
+            grid.add(gameButtons[i], i % 3, i / 3);
+        }
+    }
+
+    private void setupVBox() {
+        // Apply styles
+        
+        headerText.setFont(Font.font(24));
+        this.setSpacing(10);
+        this.setAlignment(Pos.CENTER);
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        
+        grid.setPrefHeight(500);
+        grid.setPrefWidth(500);
+
+        // Add objects
+        this.getChildren().addAll(headerText, grid);
     }
 }
--- src/game/TicTacToe.java
+++ src/game/TicTacToe.java
@@ -1,8 +1,12 @@
 package game;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TicTacToe extends Pane {
@@ -14,19 +18,32 @@ public class TicTacToe extends Pane {
 	private BorderPane gameScreen;
 	private Stage primaryStage;
 	private GameController gameController;
+	private HBox header;
+	Text headerText;
 	
 	public TicTacToe() {
+		// all game objects instantiated here
 		gameScreen = new BorderPane();
 		gameController = new GameController();
+		header = new HBox();
+		headerText = new Text("TicTacToe!");
 
 		setupHeader();
-		setupGameScreen();
+		setupPane();
 		showGame();
 	}
 	
-	
+	private void setupHeader() {
+		// Style objectgs
+		header.setAlignment(Pos.CENTER);
+		headerText.setFont(new Font(36));
 
-	private void setupGameScreen() {
+		// Add objects
+		header.getChildren().addAll(headerText);
+	}
+
+	private void setupPane() {
+		gameScreen.setTop(header);
 		gameScreen.setCenter(gameController);
 	}
 
