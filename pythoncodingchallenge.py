cfe51e166134e7a145bc5ab6ced9e5de4b53aa2d
Chris Mangan <120677529+chrisjm66@users.noreply.github.com>
2025-02-04 11:41:08 -0500

    Add files via upload

---
+++ pythoncodingchallenge.py
@@ -0,0 +1,20 @@
+import random
+
+def main():
+    num = random.randrange(1,11)
+    phrase1 = "This is the most common response!"
+    phrase2 = "epic guitar solo goes here."
+    phrase3 = "shinji get in the robot shinji."
+    phrase4 = "JACKBOT BABYYYYYYYYYYYYYY."
+
+    userinput = input("How are you?")
+    if(num <= 4):
+        print(phrase1)
+    elif(num <=6 and num > 4):
+        print(phrase2)
+    elif(num <= 8 and num >=7):
+        print(phrase3)
+    else:
+        print(phrase4)
+
+main()
